ca428584653c12725fa179d5a4eb2a14ad7fc995
BuildTools <unconfigured@null.spigotmc.org>
2021-07-29 18:35:34 +1000

    Changed

--- src/examples/java/ChangeValue.java
+++ src/examples/java/ChangeValue.java
@@ -1,16 +1,20 @@
 import me.joshua.windows.registry.RegKey;
 import me.joshua.windows.registry.RegValue;
+import me.joshua.windows.registry.types.RegQword;
+import me.joshua.windows.registry.types.RegSz;
 
 public class ChangeValue {
     public static void main(String[] args) {
         final RegKey key = new RegKey("HKEY_CURRENT_USER\\JavaRegistryLibrary\\Test");
         final RegValue myValue = key.getValue("MyValue");
 
-        myValue.asRegSz().setData("This is my value");
-        System.out.println(myValue.asRegSz().data());
+        final RegSz myValueAsRegSz = myValue.asRegSz();
+        myValueAsRegSz.setData("This is my value");
+        System.out.println(myValueAsRegSz.data());
 
         // Changing type and data
-        myValue.asRegQword().setData(666);
-        System.out.println(myValue.asRegQword().data());
+        final RegQword myValueAsRegQword = myValue.asRegQword();
+        myValueAsRegQword.setData(666);
+        System.out.println(myValueAsRegQword.data());
     }
 }
